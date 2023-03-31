f75f8c372b97cfda9ea6a90b75aace79259d5175
Johnathon Forbush <jtforbush429@gmail.com>
2023-03-31 10:24:25 -0500

    Made a simple counter

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,8 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./Components/Header";
+import Counter from './Components/Counter';
+import './Counter.css';
 
 function App() {
-  
+  return(
+    <>
+    <div className="main">
+        <Header />
+        <Counter />
+    </div>
+    </>
+  )
 }
 
 export default App;
